18e4c506b1ab137dc6729d97dee7f5700a33d3ae
andreastimothy <andreastimothyy@gmail.com>
2024-03-07 13:13:22 +0700

    refactor: make CategoryItem and Directory components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,7 @@
-import "./categories.styles.scss";
+import Directory from "./components/Directory";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return (
-    <div className="page-container">
-      <div className="categories-container">
-        {categories.map((category) => (
-          <div className="category-container">
-            <div
-              className="background-image"
-              style={{ backgroundImage: `url(${category.imageUrl})` }}
-            />
-            <div className="category-title">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <Directory />;
 };
 
 export default App;
---
+++ src/components/Directory/index.tsx
@@ -0,0 +1,9 @@
+import CategoryItem from "../CategoryItem";
+
+export default function Directory() {
+  return (
+    <div className="page-container">
+      <CategoryItem />
+    </div>
+  )
+}
